cf1324dff1dd6f9f5ee97392d2808d3f9f5ff13f
llaltluang <54599413+llaltluang@users.noreply.github.com>
2023-10-03 22:50:29 +0000

    creating forEach loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,51 @@
-let pokemonList = [
-    { name:'Bulbasaur', type:['Grass','Poison'], species:'Seed',  height:0.7},
-       
-    {  name:'Pikachu',type:['Eledtric'],species:'Mouse',height: 0.4},
-    
-    { name:'Wigglytuff', type:['Balloon'], species:'Fairy',height: 1},
-]
-//creating a loop to display each item in the pokemonList above
-for (var i = 0; i < pokemonList.length; i++) {
-document.write(`${pokemonList[i].name} (height: ${pokemonList[i].height}) `)
-if (pokemonList[i].height > 0.5) {
-    document.write(' Wow that\'s big!<br>');
-}
-  else {
-    document.write('<br>')
-  }
-}
+let pokemonRepository = (function() {
+    let pokemonList = [
+        {
+          name: "Bulbasaur",
+          types: ["Grass", "Poison"],
+          species: "Seed",
+          height: 0.7
+        },
+        { 
+          name: "Pikachu", 
+          types: ["Eledtric"], 
+          species: "Mouse", 
+          height: 0.4 
+        },
+        { name: "Wigglytuff", 
+          types: ["Balloon"], 
+          species: "Fairy", 
+          height: 1 
+        }
+    ];
 
+    function getAll() {
+        return pokemonList;
+    }
+
+    function add() {
+        pokemonList.push(pokemon);
+    }
+
+    return{
+        getAll : getAll,
+        add : add,
+    }
+
+})();
+
+// creating forEach loops
+
+pokemonRepository.getAll().forEach(function(pokemon){
+
+     if (pokemon.height > 1.5) {
+        // if the pokemon height is over 1,5 then call out special large size
+        document.write(pokemon.name);
+        document.write("(height: " + pokemon.height + ") - Wow, that's big!");
+        document.write("<br>");
+     } else {
+        document.write(pokemon.name + "(height " + ")" + "<br>");
+     }
+
+
+});
