d6efadd4b41ac178552913da512a870a502d2f29
llaltluang <54599413+llaltluang@users.noreply.github.com>
2023-10-03 22:56:18 +0000

    Ex-1.6  DOM manipulation

--- js/scripts.js
+++ js/scripts.js
@@ -1,51 +1,65 @@
-let pokemonRepository = (function() {
-    let pokemonList = [
-        {
-          name: "Bulbasaur",
-          types: ["Grass", "Poison"],
-          species: "Seed",
-          height: 0.7
-        },
-        { 
-          name: "Pikachu", 
-          types: ["Eledtric"], 
-          species: "Mouse", 
-          height: 0.4 
-        },
-        { name: "Wigglytuff", 
-          types: ["Balloon"], 
-          species: "Fairy", 
-          height: 1 
+
+//Creating IIFE 
+let pokemonRepository = (function(){
+    //Pokemon array
+      let repository = [
+        { name:'Bulbasaur', type:['Grass','Poison'], species:'Seed',  height:0.7},
+           
+        {  name:'Pikachu',type:['Eledtric'],species:'Mouse',height: 0.4},
+        
+        { name:'Wigglytuff', type:['Balloon'], species:'Fairy',height: 1}
+      ];
+      //add() function for adding pokemonlist
+      function add(pokemon){
+        //validation of proper data input
+        if (
+          typeof pokemon === 'object' &&
+          'name' in pokemon &&
+          'height' in pokemon &&
+          'types' in pokemon 
+          
+        ){
+          repository.push(pokemon);
+        } else {
+          console.log('pokemon is not correct');
         }
-    ];
-
-    function getAll() {
-        return pokemonList;
-    }
-
-    function add() {
-        pokemonList.push(pokemon);
-    }
-
-    return{
+      }
+      //getAll() function to get pokemonList
+      function getAll(){
+        return repository;
+      }
+      
+      function addListItem(pokemo){
+        let pokemonList = document.querySelector('.pokemon-list');
+        let listpokemon = document.createElement('li');
+        let button = document.createElement('button');
+        button.innerText = pokemo.name;
+        button.classList.add('button-class');
+        listpokemon.appendChild(button);
+        pokemonList.appendChild(listpokemon);
+        button.addEventListener('click',() => showDetails(pokemo));
+      };
+    
+      return{
         getAll : getAll,
         add : add,
+        addListItem : addListItem
+      };
+    
+    function showDetails(pokemo){
+      console.log(pokemo);
     }
-
-})();
-
-// creating forEach loops
-
-pokemonRepository.getAll().forEach(function(pokemon){
-
-     if (pokemon.height > 1.5) {
-        // if the pokemon height is over 1,5 then call out special large size
-        document.write(pokemon.name);
-        document.write("(height: " + pokemon.height + ") - Wow, that's big!");
-        document.write("<br>");
-     } else {
-        document.write(pokemon.name + "(height " + ")" + "<br>");
-     }
-
-
-});
+    
+    })();
+    // Adding more pokemon objects
+    
+      console.log(pokemonRepository.getAll());
+      pokemonRepository.add({ name: "Squirtle", height: 1.08, types:["water"], species:'Tiny Turtle'});
+    
+    //forEach loop to iterate through PokeDex
+    
+    pokemonRepository.getAll().forEach(function(pokemon) {
+    
+      pokemonRepository.addListItem(pokemon);   
+    });
+    
