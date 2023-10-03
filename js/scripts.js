04bace4c6e8f82425993b1eeca4ed80a5770e65b
llaltluang <54599413+llaltluang@users.noreply.github.com>
2023-10-03 22:48:02 +0000

    Ex-1.3 conditionals and loops

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,18 @@
+let pokemonList = [
+    { name:'Bulbasaur', type:['Grass','Poison'], species:'Seed',  height:0.7},
+       
+    {  name:'Pikachu',type:['Eledtric'],species:'Mouse',height: 0.4},
+    
+    { name:'Wigglytuff', type:['Balloon'], species:'Fairy',height: 1},
+]
+//creating a loop to display each item in the pokemonList above
+for (var i = 0; i < pokemonList.length; i++) {
+document.write(`${pokemonList[i].name} (height: ${pokemonList[i].height}) `)
+if (pokemonList[i].height > 0.5) {
+    document.write(' Wow that\'s big!<br>');
+}
+  else {
+    document.write('<br>')
+  }
+}
 
-let pokemonlist = [
-    {
-        name:'Bulbasaur',
-        type:['Grass','Poison'],
-        species:'Seed',
-        height:0.7,
-    },
-    {
-        name:'Pikachu',
-        type:['Eledtric'],
-        species:'Mouse',
-        height: 0.4,
-    },
-    {
-        name:'Wigglytuff',
-        type:['Balloon'],
-        species:'Fairy',
-        height: 1,
-    },
-
-  ]
