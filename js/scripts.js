854f7607aebaa36188cb8124aba8c1fce15cb7f5
llaltluang <54599413+llaltluang@users.noreply.github.com>
2023-10-03 22:46:02 +0000

    Ex-1.2 Creating data to arrays

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,22 @@
-alert('Hello World')
-let favoriteFood = 'sushi';
-document.write(favoriteFood);
+
+let pokemonlist = [
+    {
+        name:'Bulbasaur',
+        type:['Grass','Poison'],
+        species:'Seed',
+        height:0.7,
+    },
+    {
+        name:'Pikachu',
+        type:['Eledtric'],
+        species:'Mouse',
+        height: 0.4,
+    },
+    {
+        name:'Wigglytuff',
+        type:['Balloon'],
+        species:'Fairy',
+        height: 1,
+    },
+
+  ]
